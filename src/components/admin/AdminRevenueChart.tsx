dfc90fe6e1d8c853f69102c32b280963727c407e
Paras Gupta <parasgupta4494@gmail.com>
2026-02-15 22:08:21 +0530

    done

--- src/components/admin/AdminRevenueChart.tsx
+++ src/components/admin/AdminRevenueChart.tsx
@@ -92,7 +92,7 @@ export function AdminRevenueChart({ orders }: AdminRevenueChartProps) {
         <ResponsiveContainer width="100%" height="100%">
           <AreaChart
             data={chartData}
-            margin={{ top: 10, right: 10, left: 0, bottom: 0 }}
+            margin={{ top: 10, right: 10, left: -20, bottom: 0 }}
           >
             <defs>
               <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
