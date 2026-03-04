3286490de6ad84e7c5cae18be98e81828e1d0c6f
Paras Gupta <parasgupta4494@gmail.com>
2026-03-04 22:20:07 +0530

    Update text-hover-effect.tsx

--- src/components/ui/text-hover-effect.tsx
+++ src/components/ui/text-hover-effect.tsx
@@ -32,7 +32,7 @@ export const TextHoverEffect = ({
             ref={svgRef}
             width="100%"
             height="100%"
-            viewBox="0 0 600 100"
+            viewBox="0 0 800 120"
             xmlns="http://www.w3.org/2000/svg"
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
@@ -85,7 +85,7 @@ export const TextHoverEffect = ({
                 textAnchor="middle"
                 dominantBaseline="middle"
                 strokeWidth="0.3"
-                className="fill-transparent stroke-neutral-200 font-[helvetica] text-6xl font-bold dark:stroke-neutral-800"
+                className="fill-transparent stroke-neutral-200 font-[helvetica] text-[80px] font-bold dark:stroke-neutral-800"
                 style={{ opacity: hovered ? 0.7 : 0 }}
             >
                 {text}
@@ -96,7 +96,7 @@ export const TextHoverEffect = ({
                 textAnchor="middle"
                 dominantBaseline="middle"
                 strokeWidth="0.3"
-                className="fill-transparent stroke-neutral-200 font-[helvetica] text-6xl font-bold dark:stroke-neutral-800"
+                className="fill-transparent stroke-neutral-200 font-[helvetica] text-[80px] font-bold dark:stroke-neutral-800"
                 initial={{ strokeDashoffset: 1000, strokeDasharray: 1000 }}
                 animate={{
                     strokeDashoffset: 0,
@@ -117,7 +117,7 @@ export const TextHoverEffect = ({
                 stroke="url(#textGradient)"
                 strokeWidth="0.3"
                 mask="url(#textMask)"
-                className="fill-transparent font-[helvetica] text-6xl font-bold"
+                className="fill-transparent font-[helvetica] text-[80px] font-bold"
             >
                 {text}
             </text>
